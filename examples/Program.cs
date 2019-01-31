badf5a952f092c39a652b972bb00732800ebc4d3
Radu Matei <radu@radu-matei.com>
2019-01-31 15:11:33 +0200

    Add examples program

---
+++ examples/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.IO;
+
+using Cnab.Bundle;
+using Newtonsoft.Json;
+
+namespace Examples
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            var bundle = LoadBundleFile("bundles/thick-bundle.json");
+            Console.WriteLine(bundle.ToString());
+        }
+
+        public static Bundle LoadBundleFile (string fileName)
+        {
+            using (StreamReader reader = new StreamReader(fileName))
+            {
+                var json = reader.ReadToEnd();
+                return JsonConvert.DeserializeObject<Bundle>(json);
+            }
+        }
+    }
+}
