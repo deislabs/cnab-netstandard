13695ef31829b3ee00a87808b34f2875af36e43b
Radu Matei <radu@radu-matei.com>
2019-02-24 02:26:02 +0200

    Add initial Claim

---
+++ src/Cnab.Runtime/Status.cs
@@ -0,0 +1,10 @@
+namespace Cnab.Runtime
+{
+    public static class Status
+    {
+        public static string Success = "success";
+        public static string Failure = "success";
+        public static string Underway = "underway";
+        public static string Unknown = "unknown";
+    }
+}
